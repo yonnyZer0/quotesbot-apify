12eeefe47fa9ab4c38617b32f55ba08c25066550
yonnyZer0 <janzisko@gmail.com>
2018-05-22 15:40:45 +0200

    pipeline update

--- quotesbot/pipeline-dataset.py
+++ quotesbot/pipeline-dataset.py
@@ -33,7 +33,7 @@ class QuotesbotToDataset(object):
         
     def state_to_kvstore(self):
         print('saving...')
-        os.system('sync && zip -0 -r "current_run.zip" "current_run"')
+        os.system('sync && zip -R "current_run.zip" "current_run"')
         print(os.system('cat current_run/requests.seen'))
         current_run_zip = open('current_run.zip', 'rb').read()
         print( str( current_run_zip ) )
