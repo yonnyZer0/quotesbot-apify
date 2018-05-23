7c0e504f555b3bfa35bf219524281591deba08ab
yonnyZer0 <janzisko@gmail.com>
2018-05-23 22:18:33 +0200

    update _

--- quotesbot/pipeline-dataset.py
+++ quotesbot/pipeline-dataset.py
@@ -5,7 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from ../py_apify import ApifyClient
+from .. import py_apify
+from py_apify import ApifyClient
 import os, sys
 
 class QuotesbotToDataset(object):
